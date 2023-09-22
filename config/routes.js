1e6185274038134fddfb185280a373a7afb7f6ac
JefersonT4v4res <jefersonsaj_tavares@hotmail.com>
2023-09-21 23:15:50 -0300

    :working: Fixing get response, change type information in .json, update routes in index.js

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
+// Chama a função express
 const routes = express.Router();
-const pokemons = require('./../src/data/pokemons.json');
+const pokelist = require("../src/data/pokemons.json");
 
 // Buscar dados
-routes.get('/pokemons', (request, response) => {
-    return response.json(pokemons)
-});
+routes.get("/pokemons", async (request, response) => {
 
+        return response.json({
+             pokelist
+        });
+});
 
 module.exports = routes;
--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+//Gerencia as requisições
 const express = require('express');
-const morgan = require('morgan');
-const routes = require('./config/routes');
+const morgan = require('morgan')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+//Chamar função express
+const app = express();
+//Incluir as controllers
+const pokemons = require('./config/routes');
 
-const server = express();
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
 
-server.use(morgan('dev'));
-server.use(routes);
+//criar as rotas
+app.use('/', pokemons);
 
-server.listen(3000, () => {
+
+app.listen(8080, () => {
 console.log('Server started');
 });
